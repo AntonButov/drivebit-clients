21813a649a8580d04e3100b912bcc9db97f59332
AntonButov <58133570+AntonButov@users.noreply.github.com>
2025-10-14 22:43:57 +0300

    Save changes to FiltersViewModel

--- CommonViewModels/src/commonMain/kotlin/my/drivebit/viewmodels/FiltersViewModel.kt
+++ CommonViewModels/src/commonMain/kotlin/my/drivebit/viewmodels/FiltersViewModel.kt
@@ -23,12 +23,12 @@ class FiltersViewModel {
                 filters =
                     listOf(
                         FilterItem(
-                            icon = "static/images/filter-main/car.svg",
+                            icon = "https://assets.drivebit.my/images/filter-main/car.svg",
                             title = "Все",
                             backgroundIcon = "2",
                         ),
                         FilterItem(
-                            icon = "static/images/filter-main/airplane.svg",
+                            icon = "https://assets.drivebit.my/images/filter-main/airplane.svg",
                             title = "Airports",
                             backgroundIcon = "3",
                         ),
